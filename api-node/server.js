0e1aa44a089b551f272326a0dc5ae662cf965449
Bianca <bpereiraalmeida@gmail.com>
2019-10-26 04:02:57 -0200

    configurações mongoose

--- api-node/server.js
+++ api-node/server.js
@@ -7,9 +7,11 @@ const requireDir = require('require-dir');
 const app = express();
 
 //db
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect(
-    "mongodb://192.168.99.100:27017/nodeapi", 
-    { useNewUrlParser: true }
+    "mongodb://192.168.99.100:27017/nodeapi"
 );
 requireDir('./src/models');
 
