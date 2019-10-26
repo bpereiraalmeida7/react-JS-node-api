9dd7d616afc46b64f9095cc4f65002c5f1320c50
Bianca <bpereiraalmeida@gmail.com>
2019-10-26 03:26:32 -0200

    Criação da model Product, instalação de pacotes e alteração do nome do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-react-JS-node-api
+#rocketseat-reactJS-node-api
+
+Este projeto é baseado no curso NodeJS e ReactJS da Rocketseat, como tutorial de aprendizagem das linguagens citadas.
--- api-node/server.js
+++ api-node/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+
+//iniciando aplicação:
 const app = express();
 
+//db
+mongoose.connect(
+    "mongodb://192.168.99.100:27017/nodeapi", 
+    { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//rotas:
 app.get('/', (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com.br/facebook/react-native'
+    })
     res.send('API rodando...')
 });
 
-app.listen(3003);
+app.listen(3001);
