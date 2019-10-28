f39009b95b178e381fe718f957e2ffba9159499f
Bianca <bpereiraalmeida@gmail.com>
2019-10-28 16:14:32 -0300

    crud Products

--- api-node/server.js
+++ api-node/server.js
@@ -5,26 +5,30 @@ const requireDir = require('require-dir');
 
 //iniciando aplicação:
 const app = express();
+app.use(express.json());
 
 //db
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect(
-    "mongodb://192.168.99.100:27017/nodeapi"
+    /* "mongodb://192.168.99.100:27017/nodeapi" */
+    "mongodb://localhost:27017/nodeapi"
 );
 requireDir('./src/models');
 
 const Product = mongoose.model('Product');
 
 //rotas:
-app.get('/', (req, res) => {
+
+/* app.get('/', (req, res) => {
     Product.create({
         title: 'React Native',
         description: 'Build native apps with React',
         url: 'http://github.com.br/facebook/react-native'
     })
     res.send('API rodando...')
-});
+}); */
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
